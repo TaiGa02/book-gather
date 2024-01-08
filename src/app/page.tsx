9dc3975afd07d4727dcbe0a2d2f7f6fbe403a203
TaiGa02 <d-taiga.4869@outlook.jp>
2024-01-08 19:00:04 +1000

    ルート設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,34 @@
+"use client";
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const loginHandle = () => {
+    router.push("/login");
+
+  };
+
+  const signUpHandle = () => {
+    router.push("/signup")
+  };
+
   return (
     <>
       <div className="flex flex-col min-h-screen">
-        <main className="flex-grow bg-green-400 flex items-center justify-center">
+        <main className="flex-grow bg-green-500 flex items-center justify-center">
           <div className="text-center">
             <h1 className="text-slate-200 font-bold underline lg:text-9xl md:text-8xl text-6xl mb-10 mx-1">Book Gather</h1>
             <div className="flex flex-col items-center">
-              <button className="bg-blue-400 text-slate-200 rounded-md my-2 px-4 py-1 text-center text-xl w-40 shadow-md
+              <button onClick={loginHandle} className="bg-blue-400 text-slate-200 rounded-md my-2 px-4 py-1 text-center text-xl w-40 shadow-md
               hover:bg-blue-800 transition-all duration-300">ログイン</button>
-              <button className="bg-blue-400 text-slate-200 rounded-md my-2 px-4 py-1 text-center text-xl w-40 shadow-md
+              <button onClick={signUpHandle} className="bg-blue-400 text-slate-200 rounded-md my-2 px-4 py-1 text-center text-xl w-40 shadow-md
               hover:bg-blue-800 transition-all duration-300">サインアップ</button>
-              <Link href={"/"} className="px-4 py-1 text-center text-xl text-blue-400 underline mt-2
+              <Link href={"/home"} className="px-4 py-1 text-center text-xl text-blue-400 underline mt-2
               hover:text-blue-800 transition-all duration-300">
                 ゲストとして
               </Link>
