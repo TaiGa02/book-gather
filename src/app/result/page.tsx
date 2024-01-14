cf2f7e708f6518c76ac2059dbee7941da77dc3be
TaiGa02 <d-taiga.4869@outlook.jp>
2024-01-15 00:10:32 +1000

    navバーのリンク修正

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -175,7 +175,7 @@ export default function Result() {
                             <div className="hidden md:ml-6 md:block">
                                 <div className="flex space-x-4">
                                     <Link href={"/home"} className="bg-gray-900 text-white rounded-md px-3 py-2 text-xs md:text-xs font-medium" aria-current="page">ホーム</Link>
-                                    <Link href={user_name === "guest" ? "/login": "/"} className="text-slate-100 hover:bg-blue-800 transition-all duration-300 rounded-md px-3 py-2 text-xs font-medium">{user_name === "guest" ? "ログイン": "MyPage"}</Link>
+                                    <Link href={user_name === "guest" ? "/login": "/mypage"} className="text-slate-100 hover:bg-blue-800 transition-all duration-300 rounded-md px-3 py-2 text-xs font-medium">{user_name === "guest" ? "ログイン": "MyPage"}</Link>
                                     <Link href={user_name === "guest" ? "/signup": "/"} className="text-slate-100 hover:bg-slate-100 transition-all duration-300 hover:text-red-500 rounded-md px-3 py-2 text-xs font-medium">{user_name === "guest" ? "サインアップ": "ログアウト"}</Link>
                                 </div>
                             </div>
@@ -205,7 +205,7 @@ export default function Result() {
                 <div className="md:hidden flex flex-col" id="mobile-menu" style={{ display: isMenuOpen ? 'block' : 'none' }}>
                     <div className="space-y-1 px-2 pb-3 pt-2">
                         <Link href={"/home"} className="bg-gray-900 text-white rounded-md px-3 py-2 text-base font-medium" aria-current="page">ホーム</Link>
-                        <Link href={user_name === "guest" ? "/login": "/"} className="text-slate-100 hover:bg-blue-800 transition-all duration-300 rounded-md px-3 py-2 text-xs font-medium">{user_name === "guest" ? "ログイン": "MyPage"}</Link>
+                        <Link href={user_name === "guest" ? "/login": "/mypage"} className="text-slate-100 hover:bg-blue-800 transition-all duration-300 rounded-md px-3 py-2 text-xs font-medium">{user_name === "guest" ? "ログイン": "MyPage"}</Link>
                         <Link href={user_name === "guest" ? "/signup": "/"} className="text-slate-100 hover:bg-slate-100 transition-all duration-300 hover:text-red-500 rounded-md px-3 py-2 text-base font-medium">{user_name === "guest" ? "サインアップ": "ログアウト"}</Link>
                     </div>
                     <div className="px-2 pb-3 pt-2 left-0 justify-start">
