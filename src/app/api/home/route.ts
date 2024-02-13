836eea588558ecfe895a8ddb1cae20b46d95ec32
TaiGa02 <d-taiga.4869@outlook.jp>
2024-02-13 17:40:10 +1000

    月間と年間の抽出

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -6,13 +6,25 @@ const prisma = new PrismaClient();
 export const GET =async (req: Request, res: NextResponse) => {
     try {
         await prisma.$connect();
-        let currentDate = new Date();
-        let currentYear = currentDate.getFullYear();
-        let currentMonth = currentDate.getMonth();
 
         const allBooks = await prisma.book.findMany();
+        const monthlyBooks = await prisma.book.findMany({
+            where: {
+                monthly_count:{
+                    gt:0
+                }
+            }
+        });
 
-        return NextResponse.json({ message: "Success", allBooks }, {status: 200});
+        const yearlyBooks = await prisma.book.findMany({
+            where: {
+                yearly_count: {
+                    gt: 0
+                }
+            }
+        });
+
+        return NextResponse.json({ message: "Success", allBooks, monthlyBooks, yearlyBooks }, {status: 200});
         
     } catch(err){
         return NextResponse.json({ message: "Error", err}, {status: 500});
