c92f6da771e2b888ccedf1bc0d8f142219b7e18d
TaiGa02 <d-taiga.4869@outlook.jp>
2024-02-16 23:38:33 +1000

    ホームにレート表示

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -220,6 +220,14 @@ const Tabs : React.FC<TabsProps> = ({ txcolor,bgcolor }) => {
                               <div className="m-2">
                                       <p className="py-2 px-2 text-xl"><strong>タイトル:  </strong>{book.title}</p>
                                       <p className="py-1 px-2 text-xl"><strong>著者:  </strong>{book.author}</p>
+                                      <div className="flex items-center">
+                                        <div className="w-8 h-8 mr-1">
+                                          <svg className="w-full h-full text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                          </svg>
+                                        </div>
+                                        <span>{book.overall_rate}</span>
+                                      </div>
                                       <button onClick={() => handleFinish(book)} className="text-slate-100 bg-blue-400 rounded-md px-2 mx-2 my-4 hover:bg-blue-800 duration-300 transition-all">読んだ</button>
                                       <button onClick={() => handleWant(book)} className="text-slate-100 bg-blue-400 rounded-md px-2 mx-2 hover:bg-blue-800 duration-300 transition-all">気になる、読みたい</button>
                               </div>
@@ -240,6 +248,14 @@ const Tabs : React.FC<TabsProps> = ({ txcolor,bgcolor }) => {
                             <div className="m-2">
                                     <p className="py-2 px-2 text-xl"><strong>タイトル:  </strong>{book.title}</p>
                                     <p className="py-1 px-2 text-xl"><strong>著者:  </strong>{book.author}</p>
+                                    <div className="flex items-center">
+                                      <div className="w-8 h-8 mr-1">
+                                        <svg className="w-full h-full text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                        </svg>
+                                      </div>
+                                      <span>{book.overall_rate}</span>
+                                    </div>
                                     <button onClick={() => handleFinish(book)} className="text-slate-100 bg-blue-400 rounded-md px-2 mx-2 my-4 hover:bg-blue-800 duration-300 transition-all">読んだ</button>
                                     <button onClick={() => handleWant(book)} className="text-slate-100 bg-blue-400 rounded-md px-2 mx-2 hover:bg-blue-800 duration-300 transition-all">気になる、読みたい</button>
                             </div>
@@ -260,6 +276,14 @@ const Tabs : React.FC<TabsProps> = ({ txcolor,bgcolor }) => {
                             <div className="m-2">
                                     <p className="py-2 px-2 text-xl"><strong>タイトル:  </strong>{book.title}</p>
                                     <p className="py-1 px-2 text-xl"><strong>著者:  </strong>{book.author}</p>
+                                    <div className="flex items-center">
+                                      <div className="w-8 h-8 mr-1">
+                                        <svg className="w-full h-full text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                        </svg>
+                                      </div>
+                                      <span>{book.overall_rate}</span>
+                                    </div>
                                     <button onClick={() => handleFinish(book)} className="text-slate-100 bg-blue-400 rounded-md px-2 mx-2 my-4 hover:bg-blue-800 duration-300 transition-all">読んだ</button>
                                     <button onClick={() => handleWant(book)} className="text-slate-100 bg-blue-400 rounded-md px-2 mx-2 hover:bg-blue-800 duration-300 transition-all">気になる、読みたい</button>
                             </div>
